f1d6e9f4051ca9996cfb7134edfe838c6e6fac9c
adcottais <adrian@pop-os.localdomain>
2021-03-10 21:50:42 +0100

    Need to rearrange the code

--- main.c
+++ main.c
@@ -2,21 +2,37 @@
 #include <stdlib.h>
 #include "queue.h"
 #include "stack.h"
+#include <time.h> 
 
 
+int CNP(int n, int p){
+    int res;
+    if(n==p || p==0){
+        return 1;
+    }
+    else{
+        res=CNP(n-1,p)+CNP(n-1, p-1);
+    }
+}
 
 int main(){
 
-    tval_stack ni=3;
-    tval_stack pi=2;
+    tval_stack ni=5;
+    tval_stack pi=3;
     int state=0;
     t_stack *stack_memory=initStack(10);
     int end=0;
-    int ret=0;
+    /*Si on mets pas ret a 1 des le debut on va retourner 0 si on on appel CNP(n,p) avec n=p*/
+    int ret=1;
+    int res_bis=0;
+    double time_spent = 0.0;
+
+    clock_t begin = clock();
 
     while(end==0){
-        while(ni!=pi && pi!=0){
+        while(ni!=pi && pi>0){
             printf("OK 2\n");
+            printf("%d %d \n", ni, pi);
             stack(stack_memory, ni);
             stack(stack_memory, pi);
             ni=ni-1;
@@ -26,6 +42,7 @@ int main(){
             printf("OK 3\n");
             unstack(stack_memory, &pi, &state);
             unstack(stack_memory, &ni, &state);
+            printf("+1\n");
             pi=pi-1;
             ni=ni-1;
             ret++;
@@ -34,10 +51,19 @@ int main(){
             end=1;
         }
     }
-    printf("%d\n", ret);
 
+    clock_t end_process = clock();
+    printf("%d\n", ret);
+    time_spent = (double)(end_process - begin) / CLOCKS_PER_SEC;
+    printf("%f secondes\n", time_spent);
 
     freeStack(stack_memory);
 
-    
+    begin = clock();
+    res_bis=CNP(5,3);
+    end_process = clock();
+    time_spent = (double)(end_process - begin) / CLOCKS_PER_SEC;
+    printf("%f secondes\n", time_spent);
+
+    printf("%d\n", res_bis);
 }
