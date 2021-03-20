b3bb24ff4735a596bf00b843a04b3ba43f5f5770
Adrian COTTAIS <adrian.cottais@etu.uca.fr>
2021-03-20 11:54:02 +0100

    New comments

--- TP2.c
+++ TP2.c
@@ -8,27 +8,33 @@
 
 int main(){
 
-    /* Quelques tests sur la pile avec la fonction suivante:*/
-
-    /*test_stack(8);
-    test_stack(20);
-    test_stack(0);
-    test_stack(-2);*/
-
-    /* Une fonction de test pour vérifier le fonctionnement de la file*/
-
-    test_queue(8);
-    test_queue(20);
-    test_queue(0);
-    test_queue(-2);
-
-    /* Pour la partie CNP*/
+    /* Déclrations pour la partie CNP*/
 
     tval_stack ni;
     tval_stack pi;
     int res_rec=0;
     int res_iter=0;
+
+    /* Déclarations pour la gestion du temps d'éxecution des fonctions*/
+    clock_t begin;
+    clock_t end_process;
     double time_spent = 0.0;
+
+    /* Quelques tests sur la pile avec la fonction suivante: (Commentés pour ne pas surchager la sortie)*/
+
+    test_stack(8);
+    /*test_stack(20);*/
+    /*test_stack(0);*/
+    /*test_stack(-2);*/
+
+    /* Une fonction de test pour vérifier le fonctionnement de la file: (Commentés pour ne pas surchager la sortie)*/
+
+    test_queue(8);
+    /*test_queue(20);*/
+    /*test_queue(0);*/
+    /*test_queue(-2);*/
+
+    
 
     /***** Appel de la fonction CNP recursive *****/
     printf("Choisir un n pour la fonction CNP: \n");
@@ -37,9 +43,16 @@ int main(){
     scanf(FORMAT_VAL, &pi);
 
     if(ni>=pi && pi>=0){
-        clock_t begin = clock();
+
+        /***** Appel de la fonction CNP recursive *****/
+
+        /*Relevé du temps de départ*/
+        begin = clock();
         res_rec=CNP_REC(ni, pi);
-        clock_t end_process = clock();
+
+        /* Relevé du temps de fin*/
+        end_process = clock();
+        
         time_spent = (double)(end_process - begin) / CLOCKS_PER_SEC;
         printf("\nTemps de process: %f secondes\n", time_spent);
         printf("Resultat par la fonction recursive est %d\n", res_rec);
